bacad39d26eba086a669b09248556ca802cc65da
NITHIN_AAKASH <39897089+nithinaakash@users.noreply.github.com>
2025-04-25 09:49:59 -0400

    Update flightlabsService.ts

--- src/services/flightlabsService.ts
+++ src/services/flightlabsService.ts
@@ -5,6 +5,7 @@ import { supabase } from '../utils/supabaseClient';
 
 
 const FLIGHTLABS_API_BASE_URL = 'https://app.goflightlabs.com';
+// Manual deployment
 const FLIGHTLABS_ACCESS_KEY = process.env.NEXT_PUBLIC_FLIGHTLABS_API_KEY;
 
 const flightLabsRequest = async (endpoint, params) => {
